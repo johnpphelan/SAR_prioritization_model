ad7cd4d1bb0c1b541d31a714366052bb56b9e478
Madsen <Chris.Madsen@gov.bc.ca>
2025-02-11 11:58:23 -0800

    Foot in the door exclamation mark

--- make_prioritization_model_excel_output.R
+++ make_prioritization_model_excel_output.R
@@ -1,125 +1,157 @@
 # Use this 'for' loop to run the entire script.
-for(m in 1){
-  
-  library(openxlsx)
-  library(tidyverse)
-  library(bcdata)
-  library(sf)
-  library(DBI)
-  library(bcinvadeR)
-  library(terra)
-  library(geodata)
-  library(predicts)
-  library(ggpubr)
-  library(dismo)
-  library(rJava)
-  library(patchwork)
-  library(ecospat)
-  # If you don't have this package installed, grab it from Chris' github page:
-  #devtools::install_github('chrispmad/fwa.connect')
-  library(fwa.connect)
-  # library(ENMeval)
-  
-  # =========================================
-  
-  # Get / set file paths
-  proj_wd = getwd()
-  onedrive_wd = paste0(str_extract(getwd(),"C:/Users/[A-Z]+/"),"OneDrive - Government of BC/data/")
-  lan_root = "//SFP.IDIR.BCGOV/S140/S40203/RSD_ FISH & AQUATIC HABITAT BRANCH/General/"
-  output_folder = paste0(lan_root,"2 SCIENCE - Invasives/GENERAL/Budget/Canada Nature fund 2023-2026/Work Planning and modelling/")
-  maxent_output_folder = paste0(output_folder,"MaxEnt_predictions/")
-  
-  ## Remove rows where SARA overlaps are lacking?
-  remove_zero_sara = TRUE
-  
-  # Read in utility scripts for functions
-  source('scripts/utils/gather_AIS_data.R')
-  source('scripts/utils/native_CDC_COSEWIC_SARA_species_occurrence_counter.R')
-  source('scripts/utils/read_in_inputs_and_clean_for_model_excel_output.R')
-  source('scripts/utils/add_occurrence_record_fields.R')
-  source("scripts/utils/native_CDC_COSEWIC_SARA_species_occurrence_counter.R")
-  source("scripts/utils/add_first_nations_territories_and_wildlife_habitat_areas_overlaps.R")
-  source("scripts/utils/add_introduction_risk.R")
-  source("scripts/utils/add_maxent_prediction_vars.R")
-  source("scripts/utils/summarise_columns_and_produce_excel_output_file.R")
-  source("scripts/utils/add_sara_overlap_wbs_in_10_km_variable.R")
-  source("scripts/utils/count_trailing_zeros.R")
-  source("scripts/utils/find_downstream_waterbody.R")
-  source("scripts/utils/join_sara_cdc_to_wb.R")
-  # list of invasive species on our watch list.
-  pr_sp = gather_ais_data(data = 'species list', lan_root = lan_root, onedrive_wd = onedrive_wd)
-  
-  # Make BC shapefile.
-  bc = bcmaps::bc_bound() |> 
-    sf::st_transform(4326) |> 
-    terra::vect()
-  
-  # Grab regions of BC. These will be useful for finding waterbodies.
-  regs = bcmaps::nr_regions() |> sf::st_transform(4326)
-  
-  # =========================================
-  # Read in data and clean it up a bit
-  data_files = read_in_inputs_and_clean_for_model_excel_output(onedrive_wd)
-  d = data_files[[1]]
-  unique_wbs = data_files[[2]]
-  wbs_overlap_sara_cdc = data_files[[3]]
-  
-  # =========================================
-  
-  # File that tracks previously performed queries / runs of these script, to save time.
-  if(file.exists(paste0(onedrive_wd,"AIS_previous_query_results.rds"))){
-    previous_results = readRDS(file = paste0(onedrive_wd,"AIS_previous_query_results.rds"))
-  } else {
-    previous_results = d |> 
-      sf::st_drop_geometry() |> 
-      dplyr::select(Region:FWA_WATERSHED_CODE) |> 
-      dplyr::mutate(query_date = Sys.Date()) |> 
-      dplyr::select(query_date, dplyr::everything())
-    saveRDS(previous_results, paste0(onedrive_wd,"AIS_previous_query_results.rds"))
-  }
-  
-  # ================================
-  # Bring in / calculate variables
-  
-  # 1. Calculate number of inflows and outflows using fwa.connect package!!
-  tbl_of_fwa_junctions = fwa.connect::stream_conn_tbl()
-  # The following function takes about 1-2 minutes.
-  d = d |> 
-    rowwise() |> 
-    dplyr::mutate(number_outflows = nrow(tbl_of_fwa_junctions[tbl_of_fwa_junctions$upstream_fwa_code == FWA_WATERSHED_CODE,]),
-                  number_inflows = nrow(tbl_of_fwa_junctions[tbl_of_fwa_junctions$downstream_fwa_code == FWA_WATERSHED_CODE,])) |> 
-    dplyr::ungroup()
-  
-  # Additional test - look for waterbodies within x KM that are connected
-  # to focal waterbody and that have SAR overlaps
-  print("Adding in overlaps with SAR waterbodies that are within 10 km of target waterbodies")
-  sara_overlap_wbs = sf::read_sf(paste0(onedrive_wd,"waterbodies_overlapping_with_SARA_and_CDC_occs.gpkg"))
-  d = add_sara_overlap_wbs_in_10_km(d,sara_overlap_wbs,previous_results)
-  
-  # 2. Number of records in waterbody
-  print("Adding in occurrence record fields")
-  d = add_occurrence_record_fields(d,lan_root)
-  
-  # 3. Distinct DFO SARA species, COSEWIC and CDC species in waterbody.
-  # previous_results = readRDS(file = paste0(onedrive_wd,"AIS_previous_query_results.rds"))
-  print("Pulling in native, CDC, SARA and COSEWIC occurrences")
-  #View(readRDS(paste0(onedrive_wd,"AIS_previous_query_results.rds")))
-  d = native_CDC_COSEWIC_SARA_species_occurrence_counter(d,unique_wbs,wbs_overlap_sara_cdc,onedrive_wd)
-  
-  # 4. MaxEnt Predictions
-  print("Adding maxent prediction variables")
-  d = add_maxent_prediction_vars(d,onedrive_wd,maxent_output_folder)
-  
-  # 5. Overlaps with First Nations Territories and also Wildlife Habitat Areas
-  previous_results = readRDS(file = paste0(onedrive_wd,"AIS_previous_query_results.rds"))
-  print("Doing overlap with First Nations Territories and Wildlife Habitat Areas")
-  d = add_first_nations_territories_and_wildlife_habitat_areas_overlaps(d,unique_wbs,onedrive_wd)
-  
-  # previous_results = readRDS(file = paste0(onedrive_wd,"AIS_previous_query_results.rds"))
-  print("Adding introduction Risk variables")
-  d = add_introduction_risk(d, lan_root)
-  # =========================================
-  # Make bins for variables
-  print("Summarising output to Excel file!")
-  summarise_columns_and_produce_excel_output_file(d,output_folder,maxent_output_folder,remove_zero_sara)
+
+library(openxlsx)
+library(tidyverse)
+library(bcdata)
+library(sf)
+library(DBI)
+library(bcinvadeR)
+library(terra)
+library(geodata)
+library(predicts)
+library(ggpubr)
+library(dismo)
+library(rJava)
+library(patchwork)
+library(ecospat)
+# If you don't have this package installed, grab it from Chris' github page:
+#devtools::install_github('chrispmad/fwa.connect')
+library(fwa.connect)
+# library(ENMeval)
+
+# =========================================
+
+# 1 - LOAD DATA -----------------------------
+# Get / set file paths
+source("scripts/utils/load_data.R")
+
+# SAR stuff
+if(!file.exists("data/dfo_sar_w_wb_no_dups.rds")){
+
+  dfo_sar_frascol = dfo_sar |>
+    sf::st_filter(frascol)
+
+  rm(dfo_sar)
+
+  dfo_sar_w_wb = dfo_sar_frascol |>
+    dplyr::select(-Waterbody) |>
+    sf::st_join(named_wbs)
+
+  # Drop geometries!
+  dfo_sar_no_geom = dfo_sar_w_wb |>
+    sf::st_drop_geometry()
+
+  # Some duplications of SAR by waterbody.
+  dfo_sar_w_wb_no_dups = dfo_sar_no_geom |>
+    dplyr::mutate(Population_EN = tidyr::replace_na(Population_EN, "NA")) |>
+    dplyr::group_by(waterbody, BLUE_LINE_KEY, watershed, FWA_WATERSHED_CODE, wb_type) |>
+    dplyr::summarise(Common_Name_EN = paste0(Common_Name_EN, collapse = ', '),
+                     Population_EN = paste0(na.omit(Population_EN), collapse = ', '),
+                     Scientific_Name = paste0(Scientific_Name, collapse = ', '),
+                     .groups = 'drop')
+
+  # We've got some repeated species in a single waterbody! Drop those.
+  dfo_sar_w_wb_no_dups = dfo_sar_w_wb_no_dups |>
+    tidyr::separate_longer_delim(cols = c(Common_Name_EN:Scientific_Name), delim = ', ') |>
+    dplyr::group_by(waterbody, BLUE_LINE_KEY, watershed, FWA_WATERSHED_CODE, wb_type) |>
+    dplyr::filter(!duplicated(Common_Name_EN)) |>
+    dplyr::summarise(Common_Name_EN = paste0(Common_Name_EN, collapse = ', '),
+                     Population_EN = paste0(na.omit(Population_EN), collapse = ', '),
+                     Scientific_Name = paste0(Scientific_Name, collapse = ', '),
+                     .groups = 'drop')
+
+  saveRDS(dfo_sar_w_wb_no_dups, "data/dfo_sar_w_wb_no_dups.rds")
+
+} else {
+  dfo_sar_w_wb_no_dups = readRDS("data/dfo_sar_w_wb_no_dups.rds")
 }
+
+# AIS stuff
+ais_occ_frascol = ais_occs |>
+  sf::st_filter(frascol)
+rm(ais_occs)
+ais_occs_w_wb = ais_occ_frascol |>
+  sf::st_join(named_wbs) |>
+  dplyr::filter(!is.na(waterbody))
+# Drop geometries!
+ais_occs_no_geom = ais_occs_w_wb |>
+  sf::st_drop_geometry()
+rm(ais_occs_w_wb)
+
+
+# Some duplications of AIS by waterbody.
+ais_occs_no_dups = ais_occs_no_geom |>
+  dplyr::group_by(waterbody, BLUE_LINE_KEY, watershed, FWA_WATERSHED_CODE, wb_type) |>
+  dplyr::summarise(Species = paste0(Species, collapse = ', '),
+                   .groups = 'drop')
+
+# We've got some repeated species in a single waterbody! Drop those.
+ais_occs_no_dups = ais_occs_no_dups |>
+  tidyr::separate_longer_delim(cols = Species, delim = ', ') |>
+  dplyr::group_by(waterbody, BLUE_LINE_KEY, watershed, FWA_WATERSHED_CODE, wb_type) |>
+  dplyr::filter(!duplicated(Species)) |>
+  dplyr::summarise(Species = paste0(Species, collapse = ', '),
+                   .groups = 'drop')
+
+# 2 - Overlap SAR with AIS ======================
+dfo_sar_w_ais = dfo_sar_w_wb_no_dups |>
+  dplyr::left_join(ais_occs_no_dups, by = join_by(waterbody,BLUE_LINE_KEY,watershed,FWA_WATERSHED_CODE, wb_type))
+
+# 3. Calculate some columns =====================
+
+dfo_sar_w_ais = dfo_sar_w_ais |>
+  dplyr::mutate(ais_present_in_wb = !is.na(Species)) |>
+  dplyr::rename(ais_present_names = Species) |>
+  dplyr::mutate(number_ais_present = stringr::str_count(ais_present_names,", ") + 1) |>
+  dplyr::mutate(number_ais_present = tidyr::replace_na(number_ais_present, 0))
+
+dfo_sar_w_ais$ais_upstream = FALSE
+dfo_sar_w_ais$ais_upstream_names = ""
+dfo_sar_w_ais$ais_upstream_number = 0
+dfo_sar_w_ais$sum_of_maxent_hab_not_hab = 0
+
+for(i in 1:nrow(dfo_sar_w_ais)){
+  print(i)
+  row_name = dfo_sar_w_ais$waterbody[i]
+  row_fwa = dfo_sar_w_ais$FWA_WATERSHED_CODE[i]
+  row_fwa_prefix = str_remove(row_fwa, "000000.*")
+  row_wb_type = dfo_sar_w_ais$wb_type[i]
+  row_watershed = dfo_sar_w_ais$watershed[i]
+
+  # Filter named waterbodies for just those upstream of the_wb.
+  # Then, do AIS overlap with just those upstream wb(s).
+  upstream_named_wbs = named_wbs |>
+    dplyr::filter(str_detect(FWA_WATERSHED_CODE,paste0(row_fwa_prefix))) |>
+    dplyr::filter(FWA_WATERSHED_CODE != row_fwa) |>
+    dplyr::filter(str_detect(FWA_WATERSHED_CODE,paste0(row_fwa_prefix,"(?!000000)"))) |>
+    dplyr::filter(str_detect(FWA_WATERSHED_CODE,paste0(row_fwa_prefix,"[0-9]{6}-000000"))) |>
+    sf::st_drop_geometry()
+
+  upstream_named_wbs = upstream_named_wbs |>
+    dplyr::filter(watershed == row_watershed)
+
+  ais_upstream = upstream_named_wbs |>
+    dplyr::select(-wb_type) |>
+    dplyr::inner_join(ais_occs_no_dups, by = join_by(waterbody,BLUE_LINE_KEY,watershed,FWA_WATERSHED_CODE)) |>
+    dplyr::select(-FWA_WATERSHED_CODE) |>
+    tidyr::separate_longer_delim(cols = Species, delim = ', ') |>
+    dplyr::select(Species) |>
+    dplyr::distinct()
+
+  dfo_sar_w_ais$ais_upstream[i] = as.logical(nrow(ais_upstream) > 0)
+  dfo_sar_w_ais$ais_upstream_names[i] = paste0(ais_upstream$Species, collapse = ', ')
+  dfo_sar_w_ais$ais_upstream_number[i] = nrow(ais_upstream)
+}
+
+# Drop rows where we have no AIS in waterbody and also no AIS upstream!
+dfo_sar_w_ais = dfo_sar_w_ais |>
+  dplyr::filter(ais_present_in_wb | ais_upstream)
+
+# Drop some columns that we don't need for the Excel output
+dfo_output = dfo_sar_w_ais |>
+  dplyr::select(-c(FWA_WATERSHED_CODE,BLUE_LINE_KEY,wb_type))
+
+my_wb = openxlsx::createWorkbook()
+openxlsx::addWorksheet(my_wb, "output")
+openxlsx::writeData(my_wb, "output", dfo_output)
+openxlsx::saveWorkbook(my_wb, file = 'output/SARA_prioritization_model_output.xlsx', overwrite = T)
---
+++ scripts/utils/load_data.R
@@ -0,0 +1,51 @@
+proj_wd = getwd()
+onedrive_wd = paste0(str_extract(getwd(),"C:/Users/[A-Z]+/"),"OneDrive - Government of BC/data/")
+lan_root = "//SFP.IDIR.BCGOV/S140/S40203/RSD_ FISH & AQUATIC HABITAT BRANCH/General/"
+output_folder = paste0(lan_root,"2 SCIENCE - Invasives/GENERAL/Budget/Canada Nature fund 2023-2026/Work Planning and modelling/")
+maxent_output_folder = paste0(output_folder,"MaxEnt_predictions/")
+fras = sf::read_sf(paste0(onedrive_wd,"CNF/fraser_watershed_priority_area.gpkg"))
+col = sf::read_sf(paste0(onedrive_wd,"CNF/columbia_watershed_priority_area.gpkg"))
+frascol = dplyr::bind_rows(fras,col) |> dplyr::summarise() |> sf::st_transform(4326)
+# Read in SARA-listed species plus Sockeye Salmon (for all of BC)
+dfo_sar = sf::read_sf(paste0(onedrive_wd,"CNF/dfo_sara_and_crit_hab_and_sockeye_data.gpkg")) |> sf::st_transform(4326)
+# list of invasive species on our watch list.
+ais_occs = sf::read_sf(paste0(lan_root, "2 SCIENCE - Invasives/SPECIES/5_Incidental Observations/AIS_occurrences_all_sources.gpkg")) |>
+  sf::st_transform(4326)
+# Make BC shapefile.
+bc = bcmaps::bc_bound() |>
+  sf::st_transform(4326) |>
+  terra::vect()
+# Grab regions of BC. These will be useful for finding waterbodies.
+regs = bcmaps::nr_regions() |> sf::st_transform(4326)
+# ggplot() + geom_sf(data = dfo_sar, aes(fill = Common_Name_EN, col = Common_Name_EN))
+# named_wbs = sf::read_sf("W:/CMadsen/shared_data_sets/summarized_bc_waterbodies.shp") |>
+#   dplyr::select(waterbody = GNIS_NAME_, watershed = WATERSHED_, BLUE_LINE_KEY = BLUE_LI) |>
+#   sf::st_transform(4326)
+
+if(!file.exists("data/named_lakes_and_rivers.rds")){
+  named_lakes = bcdc_query_geodata('freshwater-atlas-lakes') |>
+    filter(!is.na(GNIS_NAME_1)) |>
+    collect() |>
+    dplyr::select(waterbody = GNIS_NAME_1, watershed = WATERSHED_GROUP_ID, BLUE_LINE_KEY, FWA_WATERSHED_CODE) |>
+    dplyr::group_by(waterbody, watershed, BLUE_LINE_KEY, FWA_WATERSHED_CODE) |>
+    dplyr::summarise(.groups = 'drop') |>
+    dplyr::mutate(wb_type = "lake")
+
+  named_rivers = bcdc_query_geodata('freshwater-atlas-rivers') |>
+    filter(!is.na(GNIS_NAME_1)) |>
+    collect() |>
+    dplyr::select(waterbody = GNIS_NAME_1, watershed = WATERSHED_GROUP_ID, BLUE_LINE_KEY, FWA_WATERSHED_CODE) |>
+    dplyr::group_by(waterbody, watershed, BLUE_LINE_KEY, FWA_WATERSHED_CODE) |>
+    dplyr::summarise(.groups = 'drop') |>
+    dplyr::mutate(wb_type = "river")
+
+  named_wbs = dplyr::bind_rows(
+    named_lakes,
+    named_rivers
+  ) |>
+    sf::st_transform(4326)
+
+  saveRDS(named_wbs, "data/named_lakes_and_rivers.rds")
+} else {
+  named_wbs = readRDS("data/named_lakes_and_rivers.rds")
+}
